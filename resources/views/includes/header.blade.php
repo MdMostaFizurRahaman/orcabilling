d3d0ee5e11db173e854d6a768cd9256f79e5f705
Shahadat Hossen <shobuj@bansberrysg.com>
2020-02-14 17:11:40 +0600

    Bugs fixed

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -15,7 +15,7 @@
                     <!-- Dark Logo icon -->
                     <img src="{{asset('theme')}}/assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
                     <!-- Light Logo icon -->
-                    <img src="{{asset('theme')}}/assets/images/logo-light-icon.png" alt="homepage" class="light-logo" />
+                    <img src="{{asset('theme')}}/assets/images/logo-light-icon.png" width='20' height='20' alt="homepage" class="light-logo" />
                 </b>
                 <!--End Logo icon -->
                 <!-- Logo text -->
--- resources/views/pages/system/company-settings.blade.php
+++ resources/views/pages/system/company-settings.blade.php
@@ -63,10 +63,11 @@
                                                 @enderror
                                             </div>
                                         </div>
+                                        <img src="" alt="">
                                         <div class="col-sm-6" style="margin: auto">
                                             <div class="form-group @error('logo'){{'is-invalid'}}@enderror">
                                                 <label for="">Logo Image</label>
-                                                <input class="form-control dropify"  data-height="100" data-allowed-file-extensions="png jpg" type="file" name="logo" data-default-file="@if(!empty($company)){{'public/'.$company->getFirstMediaUrl('logo')}}@endif">
+                                                <input class="form-control dropify"  data-height="100" data-allowed-file-extensions="png jpg" type="file" name="logo" data-default-file="@if(!empty($company)){{asset($company->getFirstMediaUrl('logo'))}}@endif">
                                                 <small class="text-info">* Image height should be 100x32 px. </small>
                                                 @error('logo')
                                                     <div class="text-danger">{{ $message }}</div>
